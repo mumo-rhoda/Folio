799342958c6078f724d0c98db8619037195e264f
rhodamumo117@gmail.com <rhodamumo117@gmail.com>
2024-03-24 19:16:11 +0300

    Passing Data Between Components

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { Project } from '../_models/Project';
+import { Tag } from '../_models/Tag';
 @Component({
   selector: 'app-portfolio',
   templateUrl: './portfolio.component.html',
   styleUrls: ['./portfolio.component.css']
 })
 export class PortfolioComponent {
+project: Project ={
+  id: 0,
+  name: 'Sampe Angular App',
+  summary:"Test Description",
+  description:'',
+  projectlink:'',
+  tags:[Tag.ANGULAR, Tag.TYPESCRIPT],
+  pictures:[]
+
+};
+
   constructor(private titleService: Title){
     this.titleService.setTitle('Rhoda Mumo -Portfolio')
 
